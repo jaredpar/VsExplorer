60351b9c4b39ce2155160d43f94b3642a4b65686
Jared Parsons <jaredpar@microsoft.com>
2014-07-19 10:40:16 -0700

    Better single line normalization

--- Src/VsExplorer/Implementation/DocumentView/SingleLineConverter.cs
+++ Src/VsExplorer/Implementation/DocumentView/SingleLineConverter.cs
@@ -12,7 +12,31 @@ namespace VsExplorer.Implementation.DocumentView
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var text = (string)value;
-            return text.Replace(Environment.NewLine, " ");
+            var builder = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                var c = text[i];
+                switch (c)
+                {
+                    case '\r':
+                        if (i + 1 < text.Length && text[i + 1] == '\n')
+                        {
+                            i++;
+                        }
+                        builder.Append(" ");
+                        break;
+                    case '\n':
+                    case '\u2028':
+                    case '\u2029':
+                        builder.Append(" ");
+                        break;
+                    default:
+                        builder.Append(c);
+                        break;
+                }
+            }
+
+            return builder.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
